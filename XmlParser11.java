74118312fb8d0d536a15ef1340de79a32ddc67bd
studhadoop <unmesha367@gmail.com>
2013-04-01 09:01:30 +0530

    Update XmlParser11.java

--- XmlParser11.java
+++ XmlParser11.java
@@ -165,10 +165,10 @@ public class XmlParser11
           case XMLStreamConstants.CHARACTERS:  //CHARACTERS:
             if (currentElement.equalsIgnoreCase("name")) {
               propertyName += reader.getText();
-              System.out.println(propertyName);
+              //System.out.println(propertyName);
             } else if (currentElement.equalsIgnoreCase("value")) {
               propertyValue += reader.getText();
-              System.out.println(propertyValue);
+              //System.out.println(propertyValue);
             }
             break;
         }
